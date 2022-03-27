c5b5be1eb4994a48012af478c5033afef5d4f6f3
KuangdiOnTheEarth <44149122+KuangdiOnTheEarth@users.noreply.github.com>
2022-03-27 13:46:41 +0100

    Complete the function of building query plan.

--- src/main/java/ed/inf/adbs/minibase/Minibase.java
+++ src/main/java/ed/inf/adbs/minibase/Minibase.java
@@ -1,11 +1,11 @@
 package ed.inf.adbs.minibase;
 
-import ed.inf.adbs.minibase.base.Atom;
-import ed.inf.adbs.minibase.base.Query;
-import ed.inf.adbs.minibase.base.RelationalAtom;
+import ed.inf.adbs.minibase.base.*;
+import ed.inf.adbs.minibase.operator.*;
 import ed.inf.adbs.minibase.parser.QueryParser;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,14 +25,149 @@ public class Minibase {
         String inputFile = args[1];
         String outputFile = args[2];
 
-//        evaluateCQ(databaseDir, inputFile, outputFile);
+        evaluateCQ(databaseDir, inputFile, outputFile);
 
-        parsingExample(inputFile);
+//        parsingExample(inputFile);
     }
 
     public static void evaluateCQ(String databaseDir, String inputFile, String outputFile) {
-        // TODO: add your implementation
+        try {
+            System.out.println("Start Minibase query evaluation:");
+            DBCatalog dbc = DBCatalog.getInstance();
+            dbc.init(databaseDir);
+
+            Query query = QueryParser.parse(Paths.get(inputFile));
+            System.out.println("Input query: " + query);
+
+            Operator queryPlan = buildQueryPlan(query);
+            if (queryPlan != null)
+                queryPlan.dump();
+            else
+                System.out.println("-- Empty query --");
+
+        } catch (Exception e) {
+            System.err.println("Exception occurred during parsing");
+            e.printStackTrace();
+        }
     }
+
+    private static Operator buildQueryPlan(Query query) {
+        List<RelationalAtom> relationalAtoms = new ArrayList<>();
+        List<ComparisonAtom> selectConditions = new ArrayList<>();
+
+        // Get the list of appeared variable names
+        List<String> usedVariables = new ArrayList<>();
+        for (Atom atom : query.getBody()) {
+            // the variables in ComparisonAtom are permitted to appear in some RelationalAtom
+            // so no need to check the variables in ComparisonAtom
+            if (atom instanceof RelationalAtom) {
+                for (Term term : ((RelationalAtom) atom).getTerms()) {
+                    if (term instanceof Variable && !usedVariables.contains(((Variable) term).getName()))
+                        usedVariables.add(((Variable) term).getName());
+                }
+            }
+        }
+
+        // Split the body atoms into two groups: RelationalAtom and ComparisonAtom
+        // Replace the constants in RelationalAtom with new variable, and add a corresponding ComparisonAtom
+        for (Atom atom : query.getBody()) {
+            if (atom instanceof RelationalAtom) { // Handle the relationalAtom
+                // replace the constant in RelationalAtom with new variable, adding new ComparisonAtom correspondingly
+                List<Term> termList = ((RelationalAtom) atom).getTerms();
+                String relationName = ((RelationalAtom) atom).getName();
+                for (int i = 0; i < termList.size(); i++) {
+                    Term originalTerm = termList.get(i);
+                    if (originalTerm instanceof Constant) {
+                        String newVarName = generateNewVariableName(usedVariables);
+                        termList.set(i, new Variable(newVarName));
+                        selectConditions.add(new ComparisonAtom(
+                                new Variable(newVarName),
+                                originalTerm,
+                                ComparisonOperator.fromString("=")
+                        ));
+                    }
+                }
+                relationalAtoms.add(new RelationalAtom(relationName, termList));
+            } else { // Handle the ComparisonAtom
+                // store the select-condition atoms into a list
+                // identify the join conditions and store them into // together with the involved relations
+                selectConditions.add((ComparisonAtom)atom);
+            }
+        }
+
+        // Generate the query plan tree
+        Operator root = null;
+        List<String> previousVariables = new ArrayList<>();
+        for (RelationalAtom rAtom : relationalAtoms) {
+            // this variable list will be used to identify which comparisonAtom should be applied on this relationalAtom
+            List<String> subtreeVariables = new ArrayList<>();
+            for (Term term : rAtom.getTerms()) {
+                if (term instanceof Variable) subtreeVariables.add(((Variable) term).getName());
+            }
+
+            // Scan operation
+            Operator subtree = new ScanOperator(rAtom);
+
+            // Select operation
+            List<ComparisonAtom> selectCompAtomList = new ArrayList<>();
+            for (ComparisonAtom cAtom : selectConditions)
+                if (variableAllAppeared(cAtom, subtreeVariables))
+                    selectCompAtomList.add(cAtom);
+            subtree = new SelectOperator(subtree, selectCompAtomList);
+
+            // Join operation
+            List<String> mergedVariables = new ArrayList<>();
+            mergedVariables.addAll(previousVariables);
+            mergedVariables.addAll(subtreeVariables);
+            if (root == null) {
+                root = subtree;
+            } else {
+                List<ComparisonAtom> joinCompAtomList = new ArrayList<>();
+                for (ComparisonAtom cAtom : selectConditions) {
+                    if (!variableAllAppeared(cAtom, previousVariables) &&
+                            !variableAllAppeared(cAtom, subtreeVariables) &&
+                            variableAllAppeared(cAtom, mergedVariables))
+                        joinCompAtomList.add(cAtom);
+                }
+                root = new JoinOperator(root, subtree, joinCompAtomList);
+            }
+
+            // update variable list after two subtrees are joined
+            previousVariables = mergedVariables;
+        }
+        // Project operation
+        root = new ProjectOperator(root, query.getHead());
+        return root;
+    }
+
+    private static String generateNewVariableName(List<String> usedNames) {
+        int count = 0;
+        String newVar = "var" + String.valueOf(count);
+        while (usedNames.contains(newVar)) {
+            count++;
+            newVar = "var" + String.valueOf(count);
+        }
+        usedNames.add(newVar);
+        return newVar;
+    }
+
+    /**
+     * Check whether the variables in a Comparison all appeared in the variable list of a sub-tree.
+     * @param comparisonAtom
+     * @param currentVariables
+     * @return
+     */
+    private static boolean variableAllAppeared(ComparisonAtom comparisonAtom, List<String> currentVariables) {
+        if (comparisonAtom.getTerm1() instanceof Variable)
+            if (!currentVariables.contains(((Variable) comparisonAtom.getTerm1()).getName()))
+                return false;
+        if (comparisonAtom.getTerm2() instanceof Variable)
+            if (!currentVariables.contains(((Variable) comparisonAtom.getTerm2()).getName()))
+                return false;
+        return true;
+    }
+
+
 
     /**
      * Example method for getting started with the parser.
